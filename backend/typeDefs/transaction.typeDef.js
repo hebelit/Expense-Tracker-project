d50de860da910249a00dbf72f1c3a3289a92596d
unknown <ahammedkenzis@gmail.com>
2024-08-13 19:26:51 +0530

    typeDefs and resolvers

---
+++ backend/typeDefs/transaction.typeDef.js
@@ -0,0 +1,47 @@
+const transactionTypeDef = `#graphql
+type Transaction {
+    id: ID!
+    userID: ID!
+    description: String!
+    paymentType: String!
+    category: String!
+    amount: Float!
+    location: String
+    date: String!
+
+}
+
+type Query {
+    transactions:[Transaction!]
+    transaction(transactionID:ID!): Transaction
+}
+
+type Mutation {
+    createTransaction(input: CreateTransactionInput!): Transaction!
+    updateTransaction(input: UpdateTransactionInput!): Transaction!
+    deleteTransaction(transactionID:ID!): Transaction!
+}
+
+input CreateTransactionInput{
+    description: String!
+    paymentType: String!
+    category: String!
+    amount: Float!
+    location: String
+    date: String!
+}
+
+input UpdateTransactionInput{
+    transactionID:ID!
+    description: String
+    paymentType: String
+    category: String
+    amount: Float
+    location: String
+    date: String
+
+}
+
+`
+
+export default transactionTypeDef;
---
+++ backend/typeDefs/user.typeDef.js
@@ -0,0 +1,42 @@
+const userTypeDef = `#graphql
+
+type User {
+    _id:ID!
+    username:String!
+    name:String!
+    password:String!
+    profilePicture:String!
+    gender:String!
+}
+
+type Query {
+    users:[User!]
+    authUser:User
+    user(userID:ID!):User
+}
+
+type Mutation {
+    signUp(input: SignUpInput!): User
+    login(input: LogInInput!): User
+    logout: LogoutResponse
+}
+
+input SignUpInput{
+    name:String!
+    username:String!
+    password:String!
+    gender:String!
+}
+
+input LogInInput{
+    username:String!
+    password:String!
+}
+
+type LogoutResponse{
+    message:String!
+}
+
+`
+
+export default userTypeDef
